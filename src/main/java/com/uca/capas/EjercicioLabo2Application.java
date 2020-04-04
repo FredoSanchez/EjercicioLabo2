4496f0d4d8b8e9af1cb56485c1169a6d088b720f
Fredy Sánchez <00082817@uca.edu.sv>
2020-04-03 19:17:12 -0600

    Actividad del segundo laboratorio de Programacion N-Capas

---
+++ src/main/java/com/uca/capas/EjercicioLabo2Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioLabo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioLabo2Application.class, args);
+	}
+
+}
